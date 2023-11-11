e41d37be835975402ccbc76cfe33c21a8d2af7f1
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-11-11 10:47:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const axios = require("axios");
 
 app.use(bodyParser.json());
 
 // Ruta para recibir notificaciones del webhook
-app.post("/webhook-endpoint", async (req, res) => {
-  try {
-    const webhookData = req.body;
-    const comentario = webhookData.comment.body;
-    const issueKey = webhookData.issue.key;
+app.post('/webhook-endpoint', (req, res) => {
+  const webhookData = req.body;
+  cosnole.log(webhookData);
+  // Realiza acciones adicionales según tus necesidades
 
-    console.log(`Nuevo Comentario: ${comentario}`);
-
-    // Make the Axios request
-    await axios.post('https://botproto.onrender.com/comment', {
-      comentario: comentario || "no",  // Use '||' to provide a default value if comentario is falsy
-      ticket: issueKey || "nel pastel", // Use '||' to provide a default value if issueKey is falsy
-    });
-
-    res.status(200).send("Notificación recibida");
-  } catch (error) {
-    console.error("Error sending Axios request:", error);
-    res.status(500).send("Internal Server Error");
-  }
+  res.status(200).send('Notificación recibida');
 });
 
 const puerto = process.env.PORT || 3000;
